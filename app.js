b06133149d82613447470972d8d8885204c6e22b
Anthony Córdova <anthonycordova@gmail.com>
2021-06-12 20:35:59 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+
+app.set('view engine', 'ejs');
+
+app.set('views', __dirname + '/views')
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+
+app.listen(port, () => {
+    console.log("servidor corriendo en el puerto: ", port)
+})
+
+
+
+
